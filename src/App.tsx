eaa71429401e4864c36b20da2b7164040d06e624
LiuGus404 <liugushk@gmail.com>
2025-04-17 08:42:42 +0800

    updated creator

--- src/App.tsx
+++ src/App.tsx
@@ -307,9 +307,11 @@ const [facingMode, setFacingMode] = useState<'user' | 'environment'>('user');
         >
           按此進行小測驗
         </a>
-        <div className="pt-2 text-center text-xs text-gray-400 italic">
-  © 2025 Created by 4人小隊
+      </div>
+      <div className="pt-4 text-center">
+  <a href="https://forms.gle/..." ...>按此進行小測驗</a>
 </div>
+    </div>
   );
 }
 

bcd2f3fc229c12bf7bd94cee5409083262df705e
LiuGus404 <liugushk@gmail.com>
2025-05-13 10:21:27 +0800

    2.0

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import KneeBraceVideo from './KneeBraceVideo';
 import AfoVideo from './AfoVideo';
-import GuidePage from './pages/guide'; // 新增引入
+import UsageGuide from './pages/UsageGuide';
 import Layout from './layout';
 import './index.css';
 
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="/" element={<App />} />
           <Route path="/videos" element={<KneeBraceVideo />} />
           <Route path="/afo-video" element={<AfoVideo />} />
-          <Route path="/guide" element={<GuidePage />} />
+          <Route path="/guide" element={<UsageGuide />} />
         </Routes>
       </Layout>
     </BrowserRouter>
